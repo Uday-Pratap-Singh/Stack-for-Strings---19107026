90921d97332e4c80d6795e7accba41bb46e100ac
Uday Pratap Singh <81916616+Uday-Pratap-Singh@users.noreply.github.com>
2022-09-25 21:05:50 +0530

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,50 @@
+
+def createStack():
+    stack = []
+    return stack
+ 
+
+def size(stack):
+    return len(stack)
+ 
+
+def isEmpty(stack):
+    if size(stack) == 0:
+        return True
+ 
+ 
+def push(stack, item):
+    stack.append(item)
+
+
+def pop(stack):
+    if isEmpty(stack):
+        return
+    return stack.pop()
+
+ 
+def reverse(string):
+    n = len(string)
+ 
+    stack = createStack()
+ 
+    # Push all characters of string to stack
+    for i in range(0, n):
+        push(stack, string[i])
+ 
+    """Making the string empty since all
+       characters are saved in the stack"""
+    string = ""
+ 
+    """ Pop all characters of the string and
+        put them back in the string"""
+    for i in range(0, n):
+        string += pop(stack)
+ 
+    return string
+ 
+ 
+# main program 
+string = input()
+string = reverse(string)
+print("Reversed string is " + string)
